fe0c00662b2e1a0589523b79846af447c7747de4
ayrton <ayrton.parkinson1@gmail.com>
2022-12-22 08:03:21 -0500

    Add routes to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,23 @@
 import '../styles/App.css';
+import Header from './Header';
+import CreateLink from './CreateLink';
 import LinkList from './LinkList';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <LinkList />
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Routes>
+          <Route path="/" element={<LinkList/>} />
+          <Route
+            path="/create"
+            element={<CreateLink/>}
+          />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
