1aa428ea7ca0b222394d366bcbfc9ff68b5c2f7f
ayrton <ayrton.parkinson1@gmail.com>
2022-12-23 06:37:28 -0500

    Add vote UI to links

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import { AUTH_TOKEN } from '../constants';
+import { timeDifferenceForDate } from '../utils';
 
-function Link({ link }) {
+function Link({ link, index }) {
+  const authToken = localStorage.getItem(AUTH_TOKEN)
+
   return (
-    <div>
+    <div className="flex mt2 items-start">
+    <div className="flex items-center">
+      <span className="gray">{index + 1}.</span>
+      {authToken && (
+        <div
+          className="ml1 gray f11"
+          style={{ cursor: 'pointer' }}
+          onClick={() => {console.log("Clicked vote button")}}
+        >
+          ▲
+        </div>
+      )}
+    </div>
+    <div className="ml1">
       <div>
         {link.description} ({link.url})
       </div>
+      {(
+        <div className="f6 lh-copy gray">
+          {link.votes.length} votes | by{' '}
+          {link.postedBy ? link.postedBy.name : 'Unknown'}{' '}
+          {timeDifferenceForDate(link.createdAt)}
+        </div>
+      )}
     </div>
+  </div>
   );
 }
 
