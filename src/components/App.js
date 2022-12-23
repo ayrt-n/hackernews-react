d4e13f4d159814f1db8d1f9ecfb015e4527071c6
ayrton <ayrton.parkinson1@gmail.com>
2022-12-23 07:51:05 -0500

    Add routes for new and top links

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import CreateLink from './CreateLink';
 import LinkList from './LinkList';
 import Login from './Login';
 import Search from './Search';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
@@ -12,13 +12,15 @@ function App() {
       <Header />
       <div className="ph3 pv1 background-gray">
         <Routes>
-          <Route path="/" element={<LinkList/>} />
+          <Route path="/" element={<Navigate replace to="/new/1" />} />
           <Route
             path="/create"
             element={<CreateLink/>}
           />
           <Route path="/login" element={<Login/>} />
           <Route path="/search" element={<Search/>} />
+          <Route path="/top" element={<LinkList/>} />
+          <Route path="/new/:page" element={<LinkList/>} />
         </Routes>
       </div>
     </div>
